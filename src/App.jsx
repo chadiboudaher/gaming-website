f5649283e5931f77bb8d4e2a357c641456192592
chadiboudaher <chadiboudaher7@gmail.com>
2026-04-16 18:08:34 +0300

    Setting up tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1 className="text-5xl text-orange-500 font-bold text-violet-300">
+        Welcome to Awwwards
+      </h1>
+    </div>
+  );
 };
 
 export default App;
