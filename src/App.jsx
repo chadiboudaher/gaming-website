046a8bad53bd3a4fff5c26be35258e4306e9410a
chadiboudaher <chadiboudaher7@gmail.com>
2026-04-17 14:43:31 +0300

    Create Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-5xl text-orange-500 font-bold text-violet-300">
-        Welcome to Awwwards
-      </h1>
-    </div>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
+
+      <section className="z-0 min-h-screen bg-blue-500" />
+    </main>
   );
 };
 
